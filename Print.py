a2af31b78e6d599f5dfe0158b907bf6b3b3ee816
NoMatchs <3117891749@qq.com>
2026-03-23 09:11:08 +0800

    print learn

---
+++ Print.py
@@ -0,0 +1,17 @@
+# print('Hello world!')
+# print('My favorite colors are', 'blue', 'green', 'red')
+my_integer_var = 10
+print("Integer:",my_integer_var)
+
+my_float_var = 4.50
+print("my_float_var",my_float_var)
+
+my_string_var = "Hello"
+print("my_float_var",my_string_var)
+
+my_boolean_var = True
+print("Boolean",my_boolean_var)
+
+my_set_var = {7,'world',8.5}
+print("Set:",my_set_var)
+
