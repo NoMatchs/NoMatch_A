fb9a420c14ef7036c9b1471cbb1e414e6ccf5a6f
NoMatchs <3117891749@qq.com>
2026-03-30 10:20:01 +0800

    python

---
+++ Python/string.py
@@ -0,0 +1,60 @@
+# str1 = 'Hello'
+# str2 = "World"
+
+# str3 = """Multiline
+# string"""
+
+# str4 = '''Another
+# multiline
+# string'''
+
+# msg = "It's a sunny day"
+# quote = 'She said, "Hello World!"'
+
+# mag = 'It \'s a sunny day'
+# quote = "She said, \"Hello World\" "
+
+# print(mag)
+# print(quote)
+# my_str = "Hello World"
+# print('Hello' in my_str)
+# print('r' in my_str)
+# print('c' in my_str)
+
+# print(len(my_str))
+
+# greeting = 'hi'
+# greeting[0] = 'H'
+# print(greeting)
+
+# str1 = "Hello"
+# str2 = "World"
+# strPlus = str1 + " " + str2
+# print(strPlus)
+
+# name = 'john Doe'
+# age  = 21
+# name_plus_age = name + " " + str(age)
+# print(name_plus_age)
+
+# age1 = 21
+# age2 = 13
+# print(f"{age1} + {age2} = {age1 + age2}")
+
+# str1 = 'hello world'
+# print(str1[1:4])
+# print(str1[ :6])
+# print(str1[3: ])
+# print(str1[::2])
+
+str1 = 'Hello world'
+print(str1.upper())
+print(str1)
+print(str1.lower())
+
+str2 = "   haha"
+str3 = str2.strip()
+print(str3)
+print(str3.strip('a'))
+
+print(str1.replace('l','8'))
